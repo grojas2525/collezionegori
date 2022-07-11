826715a587224f449c0412ff78dc3fe7423a74a2
German <germanrojas750@gmail.com>
2022-07-11 11:00:24 +0200

    App Component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import GoriLogo from './app/assets/img/GoriLogo.png';
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-             hello nucamp!
+      <Navbar dark color='primary' sticky='top' expand='md'>
+               <Container>
+                    <NavbarBrand href='/'>
+                        <img src={GoriLogo} alt='gori logo' />
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+            Gori Collection - Pistoia, Italy
     </div>
   );
 }
